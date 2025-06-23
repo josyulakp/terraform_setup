e230b1f777faad7786c980834b68a9ae1aa91f0e
Josyula Krishna <krishna.j@perceptyne.com>
2025-06-23 19:19:28 +0530

    basic micro instance launch

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# terraform_setup
+# Terraform setup for AWS 
+
+Install aws-cli from [AWS](https://docs.aws.amazon.com/cli/latest/userguide/getting-started-install.html)  
+
+create your aws keypair pem file 
+
+```
+aws ec2 create-key-pair --key-name my-keypair --query 'KeyMaterial' --output text > ~/.ssh/my-keypair.pem
+
+##set permission so no one can read 
+chmod 400 ~/.ssh/my-keypair.pem
+
+```
+
+apply the key to your config 
+
+```
+  key_name               = "my-keypair"
+```
+
---
+++ main.tf
@@ -0,0 +1,74 @@
+# Provider Info 
+
+provider "aws" {
+    profile = "default" 
+    region = "us-east-1" 
+}
+
+# VPC
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+# Subnet
+resource "aws_subnet" "main" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+}
+
+# Internet Gateway
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+}
+
+# Route Table
+resource "aws_route_table" "rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+}
+
+# Associate Route Table with Subnet
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.main.id
+  route_table_id = aws_route_table.rt.id
+}
+
+# Security Group
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Restrict in production!
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# EC2 Instance (update your resource to use subnet and security group)
+resource "aws_instance" "app_server" {
+  ami                    = "ami-020cba7c55df1f615"
+  instance_type          = "t2.micro"
+  subnet_id              = aws_subnet.main.id
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+  key_name               = "aws-keypair"
+
+  tags = {
+    Name = "MicroInstance"
+  }
+}
